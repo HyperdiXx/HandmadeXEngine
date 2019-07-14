706c462d7e2f8ba596e099b5d600b0ae9c317a69
NZXT-H440\diXx <gamtokarev@gmail.com>
2019-07-14 11:48:19 +0300

    init build system

---
+++ buildTest.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set CompilerFlags=
+set LinkerFlags=
+
+set Platform=win64
+set BuildFlag="%~1" 
+set BuildBase=build
+set "buildDir=%BuildBase%%BuildFlag%"
+if "%~2"=="x86" set buildDir=buildWin32
+
+IF NOT EXIST "%buildDir%" mkdir "%buildDir%"
+pushd "%buildDir%"" 
+
+mkdir data
+mkdir libs
+
